9254f0e6d620664fab74dfa420941ab4dd2ff749
Darul Faturohman <29.darulfat@gmail.com>
2022-06-12 19:14:03 +0700

    Merubah tampilan halaman dan menambahkan animasi background juga mencoba menambahkan animasi karakter

---
+++ main.js
@@ -0,0 +1,5 @@
+import { Application } from "@splinetool/runtime";
+
+const canvas = document.getElementById("canvas3d");
+const app = new Application(canvas);
+app.load("https://prod.spline.design/p0-i4pp9SGQPHkiA/scene.splinecode");
